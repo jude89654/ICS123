d473654452e5ba882dfe286d026fcb60a0f6ce26
jude89654 <jude89654@gmail.com>
2015-11-11 03:09:23 +0800

    inayos ang sql queries

--- src/edu/ust/erdbms/utility/sql/SQLCommands.java
+++ src/edu/ust/erdbms/utility/sql/SQLCommands.java
@@ -4,7 +4,7 @@ public interface SQLCommands {
 
 	String ADD_ITEM= "insert into Product(date_delivered, item, manufacturer, product_code, quantity) values(?,?,?,?,?)";
 	String SEARCH_ITEM = "select * from product where product_code=?";
-	String DELETE_ITEM = "delete from product where id=?";
+	String DELETE_ITEM = "delete from product where product_code=?";
 	String GET_ALL_ITEMS = "select * from product";
 	String UPDATE_ITEM = "update product set date_delivered=?, item=?, manufacturer=?, quantity=? where product_code=?";
 	
